04f0889541672c1a9cc0c5963bd29ca1cd00e01d
343_Svetlov <21arte62@gmail.com>
2016-07-05 19:15:38 +0300

    Exercise4 completed

---
+++ Svetlov_Artem_Exercise4/Task1.js
@@ -0,0 +1,25 @@
+﻿function convolutionArray(array)
+{
+    return array.reduce(function (arr, currentarr)
+    {
+        return arr.concat(currentarr);
+    }, []);
+}
+
+function outputArray(array)
+{
+    document.write("[");
+    array.forEach(function (item)
+    {
+        document.write("[" + item + "]")
+    });
+    document.write("]");
+}
+
+function execTask1()
+{
+    var arrays = [[1, 2, 3], [4, 5], [6]];
+    document.write("Исходный массив: ");
+    outputArray(arrays);
+    document.write("<br>Полученный массив в результате слияния: " + convolutionArray(arrays));
+}
---
+++ Svetlov_Artem_Exercise4/Task4.js
@@ -0,0 +1,47 @@
+﻿function every(array, func) {
+    if (array.every(func)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function every2(array, func) {
+    for (var i = 0; i < array.length; i++) {
+        if (!func(array[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function some(array, func) {
+    if (array.some(func)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function some2(array, func) {
+    for (var i = 0; i < array.length; i++) {
+        if (func(array[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function exeTask4() {
+    document.write("Every для выражения \"[NaN, NaN, NaN], isNaN\"(1-ый способ): " + every([NaN, NaN, NaN], isNaN));
+    document.write("<br>Every для выражения \"[NaN, NaN, NaN], isNaN\"(2-ой способ): " + every2([NaN, NaN, NaN], isNaN));
+    document.write("<br>Every для выражения \"[NaN, NaN, 4], isNaN\"(1-ый способ): " + every([NaN, NaN, 4], isNaN));
+    document.write("<br>Every для выражения \"[NaN, NaN, 4], isNaN\"(2-ой способ): " + every2([NaN, NaN, 4], isNaN));
+
+    document.write("<br>Some для выражения \"[NaN, 3, 4], isNaN\"(1-ый способ): " + some([NaN, 3, 4], isNaN));
+    document.write("<br>Some для выражения \"[NaN, 3, 4], isNaN\"(2-ой способ): " + some2([NaN, 3, 4], isNaN));
+    document.write("<br>Some для выражения \"[2, 3, 4], isNaN\"(1-ый способ): " + some([2, 3, 4], isNaN));
+    document.write("<br>Some для выражения \"[2, 3, 4], isNaN\"(2-ой способ): " + some2([2, 3, 4], isNaN));
+}
