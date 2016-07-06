2fa92c503ca4c6b851de6e1721826c0074c01bf6
343_Svetlov <21arte62@gmail.com>
2016-07-06 23:19:23 +0300

    Exercise6 completed

---
+++ Svetlov_Artem_Exercise6/Task1.js
@@ -0,0 +1,34 @@
+﻿function MultiplicatorUnitFailure(message) {
+    Error.call(this);
+    this.message = message;
+    this.name = "MultiplicatorUnitFailure";
+}
+
+function primitiveMultiply(a, b) {
+    if (Math.random() < 0.5)
+        return a * b;
+    else
+        throw new MultiplicatorUnitFailure("Неповезло!!!");
+}
+
+function reliableMultiply(a, b) {
+    try {
+        return primitiveMultiply(a, b);
+    } catch (e) {
+        console.log(e.message);
+        return reliableMultiply(a, b);
+    }
+}
+
+function execTask1() {
+    var a = Number(prompt("Введите 1-ый множитель:"));
+    var b = Number(prompt("Введите 2-ой множитель:"));
+
+    if (!isNaN(a) && !isNaN(b)) {
+        document.write("Резульат умножения: " + reliableMultiply(a, b));
+    }
+    else {
+        alert("Вы ввели некорректные исходные данные!!!");
+    }
+    document.write("<br>Работа программы завершена!!!");
+}
---
+++ Svetlov_Artem_Exercise6/Task2.js
@@ -0,0 +1,50 @@
+﻿var box = {
+    locked: true,
+    unlock: function () { this.locked = false; },
+    lock: function () { this.locked = true; },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Заперто!");
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body) {
+    try {
+        box.locked = false;
+        document.write("<br>Открыл сундук.");
+        body();
+        document.write("<br>Положил " + box.content + ".");
+    } catch (e) {
+        document.write("<br>" + e.message);
+    }
+    finally {
+        box.locked = true;
+        document.write("<br>Закрыл сундук.");
+    }
+}
+
+function locked(lock) {
+    if (lock == true) {
+        return "заперт.";
+    }
+    else {
+        return "открыт.";
+    }
+}
+
+function execTask2() {
+    withBoxUnlocked(function () {
+        box.content.push("золотишко");
+    });
+
+    try {
+        withBoxUnlocked(function () {
+            throw new Error("Пираты на горизонте! Отмена!");
+        });
+    } catch (e) {
+        document.write("<br>" + e.message);
+    }
+    document.write("<br>Сундук " + locked(box.locked));
+    document.write("<br>Работа программы завершена!!!")
+}
