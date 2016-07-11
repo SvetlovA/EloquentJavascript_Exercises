caf51c693fd069419a6a149e2968eff02419084c
343_Svetlov <21arte62@gmail.com>
2016-07-11 13:55:27 +0300

    Exercise10 completed

---
+++ Svetlov_Artem_Exercise10/Task1.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var field = document.querySelector("input");
+
+    field.addEventListener("keydown", function (event) {
+        if (String.fromCharCode(event.keyCode) == 'Q' || String.fromCharCode(event.keyCode) == 'W' || String.fromCharCode(event.keyCode) == 'X') {
+            event.preventDefault();
+        }
+    });
+}());
+
---
+++ Svetlov_Artem_Exercise10/Task2.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    var images = document.getElementsByClassName("trail");
+
+    document.body.addEventListener("mousemove", function (event) {
+        for (var i = 0; i < images.length; i++) {
+            images[i].style.top = event.pageY + "px";
+            images[i].style.left = event.pageX + "px";
+        }
+    });
+
+    document.body.addEventListener("click", function (event) {
+        for (var i = 0; i < images.length; i++) {
+            images[i].style.top = event.pageY + "px";
+            images[i].style.left = event.pageX + "px";
+        }
+    });
+}());
