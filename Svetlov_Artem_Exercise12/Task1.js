daa3c5082c01b670d1e3894c9bfdb33a7adda48a
343_Svetlov <21arte62@gmail.com>
2016-07-12 14:15:38 +0300

    Exercise 12 completed

---
+++ Svetlov_Artem_Exercise12/Task1.js
@@ -0,0 +1,11 @@
+﻿function get(url, types) {
+    types.forEach(function (type) {
+        var req = new XMLHttpRequest();
+        req.open("GET", url, false);
+        req.setRequestHeader("accept", type);
+        req.send(null);
+        document.write(req.responseText + "<br>");
+    });
+}
+
+get("http://eloquentjavascript.net/author", ["text/plain", "text/html", "application/json"]);
